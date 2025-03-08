5bd4b9c789178b3dc6b8aeacb07ea38688d73e06
KhrMichael <khrmichael.github.handler495@aleeas.com>
2025-03-08 22:43:17 +0300

    fix: fix compile_commands.json deleting and linking

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIST_DIR=$(pwd)/dist
+BUILD_DIR=$(pwd)
 
 if [ ! -d "$DIST_DIR" ]; then
     mkdir -p "$DIST_DIR"
@@ -13,5 +14,5 @@ cmake -G Ninja ../..
 ninja
 
 COMPILE_COMMANDS_FILE_NAME=compile_commands.json
-rm $COMPILE_COMMANDS_FILE_NAME || true
-ln -s "$DIST_DIR/$COMPILE_COMMANDS_FILE_NAME" ../$COMPILE_COMMANDS_FILE_NAME
+rm $BUILD_DIR/../$COMPILE_COMMANDS_FILE_NAME || true
+ln -s "$DIST_DIR/$COMPILE_COMMANDS_FILE_NAME" $BUILD_DIR/../$COMPILE_COMMANDS_FILE_NAME
