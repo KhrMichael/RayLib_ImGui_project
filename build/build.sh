166346845a3a78661fb1e6e5fa4d7cd2c2e34f3a
KhrMichael <khrmichael.github.handler495@aleeas.com>
2025-03-09 00:49:04 +0300

    feat: build is copying resources directory into build/dist

--- build/build.sh
+++ build/build.sh
@@ -3,15 +3,19 @@
 DIST_DIR=$(pwd)/dist
 BUILD_DIR=$(pwd)
 
-if [ ! -d "$DIST_DIR" ]; then
-    mkdir -p "$DIST_DIR"
+if [ -d "$DIST_DIR" ]; then
+    rm -rf "$DIST_DIR"
 fi
+mkdir -p "$DIST_DIR"
 
 cd "$DIST_DIR"
 
 cmake -G Ninja ../..
 
 ninja
+
+rm -r resources
+cp -r ../../resources resources
 
 COMPILE_COMMANDS_FILE_NAME=compile_commands.json
 rm $BUILD_DIR/../$COMPILE_COMMANDS_FILE_NAME || true
