5281f04f4c5d4e63feadb18170bc4e40e63bbadd
KhrMichael <khrmichael.github.handler495@aleeas.com>
2025-03-09 22:17:15 +0300

    feat: change logging functions signatures
    logging functions now use string_view as a parameter

--- src/logging/logging.cpp
+++ src/logging/logging.cpp
@@ -1,20 +1,21 @@
 #include "logging.h"
 #include <iostream>
 
-auto HL::Logger::error(std::string message) -> void
+auto HL::Logger::error(std::string_view message) -> void
 {
     const std::string red_color = "\033[31m";
     const std::string reset_color = "\033[0m";
     std::cerr << red_color << "[ERROR]: " << reset_color << message << std::endl;
 }
 
-auto HL::Logger::warning(std::string message) -> void
+auto HL::Logger::warning(std::string_view message) -> void
 {
     const std::string gold_color = "\033[38;5;226m";
     const std::string reset_color = "\033[0m";
     std::cerr << gold_color << "[WARNING]: " << reset_color << message << std::endl;
 }
-auto HL::Logger::info(std::string message) -> void
+
+auto HL::Logger::info(std::string_view message) -> void
 {
     const std::string blue_color = "\033[1;36m";
     const std::string reset_color = "\033[0m";
--- src/logging/logging.h
+++ src/logging/logging.h
@@ -1,15 +1,15 @@
 #ifndef LOGGING_H
 #define LOGGING_H
 
-#include <string>
+#include <string_view>
 
 namespace HL
 {
     namespace Logger
     {
-        auto error(std::string message) -> void;
-        auto warning(std::string message) -> void;
-        auto info(std::string message) -> void;
+        auto error(std::string_view message) -> void;
+        auto warning(std::string_view message) -> void;
+        auto info(std::string_view message) -> void;
     } // namespace Logger
 } // namespace HL
 
