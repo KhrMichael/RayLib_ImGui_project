2649d8aac906ad28eded282d5e0bc3218503293f
Nuretdin Meredov <meredov.nurik00@gmail.com>
2025-03-12 00:33:26 +0300

    add assets and space ship Texture drawing

--- main.cpp
+++ main.cpp
@@ -2,15 +2,24 @@
 #include "imgui.h"
 #include "rlImGui.h"
 
+#include <filesystem>
+
 int main() {
 	const int winWidth = 1920 * 0.8;
 	const int winHeight = 1080 * 0.8;
 	InitWindow(winWidth, winHeight, "game_window");
 	SetTargetFPS(60);
 
+	std::filesystem::path path{ "assets" };
+	path /= "spaceship.png";
+
+	auto spaceShipTexture = LoadTexture(path.string().c_str());
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(BLACK);
+
+		DrawTexture(spaceShipTexture, 0, 0, WHITE);
 
 		EndDrawing();
 	}
